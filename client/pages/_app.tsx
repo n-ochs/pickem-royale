1d6af44a3438ab639a949bfc284f6c99d60ea19b
Nicholas Ochs <nick.ochs@yahoo.com>
2023-07-17 22:38:03 -0400

    cleanup

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -3,8 +3,6 @@ import '@styles/globals.css';
 import { useState } from 'react';
 import { Toaster } from 'react-hot-toast';
 
-import AuthCtxProvider from '@components/context/auth.context';
-// import AuthCtxProvider from '@components/context/auth.context';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
@@ -15,10 +13,8 @@ const App: (props: AppProps) => JSX.Element = ({ Component, pageProps }) => {
 
 	return (
 		<QueryClientProvider client={queryClient}>
-			<AuthCtxProvider>
-				<Component {...pageProps} />
-				<Toaster />
-			</AuthCtxProvider>
+			<Component {...pageProps} />
+			<Toaster />
 			<ReactQueryDevtools initialIsOpen={false} />
 		</QueryClientProvider>
 	);
--- client/pages/signin.tsx
+++ client/pages/signin.tsx
@@ -3,6 +3,7 @@ import { NextRouter, useRouter } from 'next/router';
 import SignInForm from '@components/auth/signin.component';
 
 import type { NextPage } from 'next';
+
 const SignIn: NextPage = () => {
 	const router: NextRouter = useRouter();
 	const from: string = (router.query?.from as string) || null;
