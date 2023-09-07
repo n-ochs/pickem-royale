135d94e0879df82dab74c69fa4ab25e83500b1f8
Nicholas Ochs <nick.ochs@yahoo.com>
2023-09-07 19:33:00 -0400

    updates api service

--- client/util/api/axios_api.service.ts
+++ client/util/api/axios_api.service.ts
@@ -1,4 +1,5 @@
-import axios, { AxiosInstance } from 'axios';
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
 import { v4 as uuid } from 'uuid';
 
 const APIService: AxiosInstance = axios.create({
@@ -15,6 +16,9 @@ APIService.interceptors.request.use((config) => {
 	return config;
 });
 
+// https://stackoverflow.com/questions/60749813/how-to-avoid-multiple-token-refresh-requests-when-making-simultaneous-api-reques
+let refreshTokenPromise: Promise<AxiosResponse<any, any>>;
+
 APIService.interceptors.response.use(
 	(response) => {
 		response = response?.data?.data;
@@ -22,12 +26,18 @@ APIService.interceptors.response.use(
 	},
 	async (error) => {
 		if (error.response?.config?.url === '/auth/refresh' && error?.response?.status === 401) {
+			window.location.replace('/');
 			return Promise.reject(error);
 		}
 
 		if (error?.response?.status === 401) {
-			await APIService.post('/auth/refresh');
-			return APIService(error?.config);
+			if (!refreshTokenPromise) {
+				refreshTokenPromise = APIService.post('/auth/refresh').then(() => (refreshTokenPromise = null));
+			}
+
+			return refreshTokenPromise.then(() => {
+				return APIService(error?.config);
+			});
 		}
 		return Promise.reject(error);
 	}
